e7046c5ae4024a3826b06c5f07b6e98c23ac7a6c
Martin Dobrev <Martin Dobrev>
2016-06-23 00:01:22 +0300

    Rework data sets loading

--- FFTNN/header/BinaryImage.h
+++ FFTNN/header/BinaryImage.h
@@ -12,17 +12,14 @@ class BinaryImage
 	{
 	}
 
-	BinaryImage(wxString &rPath)
+	BinaryImage(wxImage &rBmp)
 	{
-		wxImageHandler * bmpLoader = new wxBMPHandler();
-		wxImage::AddHandler(bmpLoader);
-		wxImage bmp(rPath, wxBITMAP_TYPE_BMP);
-		mW = bmp.GetWidth();
-		mH = bmp.GetHeight();
+		mW = rBmp.GetWidth();
+		mH = rBmp.GetHeight();
 		for (size_t i; i < mH; ++i)
 			for (size_t j; j < mW; ++j)
 			{
-				mData[i * mW + j] = ((0.299 * bmp.GetRed(i, j) + 0.587 * bmp.GetGreen(i, j) + 0.114 * bmp.GetBlue(i, j)) * 255);
+				mData[i * mW + j] = ((0.299 * rBmp.GetRed(i, j) + 0.587 * rBmp.GetGreen(i, j) + 0.114 * rBmp.GetBlue(i, j)) * 255);
 			}
 	}
 
--- FFTNN/header/LearningData.h
+++ FFTNN/header/LearningData.h
@@ -1,28 +1,45 @@
 #pragma once
 #include <wx/dir.h>
+#include <wx/log.h>
 #include <vector>
 #include "../header/BinaryImage.h"
 
 class LearningData
 {
 	public:
+		typedef std::vector<BinaryImage*> DataSet;
+		typedef std::vector<BinaryImage*>::const_iterator DataSetConstIter;
+		typedef std::vector<BinaryImage*>::iterator DataSetIter;
 		LearningData(wxString& rDirPath)
 		{
 			wxDir dir(rDirPath);
 			if (!dir.IsOpened())
 			{
-				return;// TODO ...
+				wxLogError("Cannot open dir '%s'.", rDirPath);
+				return;
 			}
-
 
 			wxString filename;
 			bool cont = dir.GetFirst(&filename, "", wxDIR_FILES);
 			while (cont)
 			{
-				dataSet.push_back(new BinaryImage(filename));
+				wxImageHandler * bmpLoader = new wxBMPHandler();
+				wxImage::AddHandler(bmpLoader);
+				wxImage bmp(filename, wxBITMAP_TYPE_BMP);
+				if (bmp.IsOk())
+					dataSet.push_back(new BinaryImage(bmp));
 				cont = dir.GetNext(&filename);
 			}
 		}
+
+		LearningData()
+		{
+			for (DataSetIter it = dataSet.begin(); it != dataSet.end(); ++it)
+				delete *it;
+		}
+
+		DataSetConstIter GetBeg() { dataSet.begin(); }
+		DataSetConstIter GetEnd() { dataSet.end(); }
 	private:
-		std::vector<BinaryImage*> dataSet;
+		DataSet dataSet;
 };
